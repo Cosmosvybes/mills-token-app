a1516fdcbf4568a8e3169243f49886c1824e4c3d
cosmos <alfredchrisayo@gmail.com>
2025-04-01 11:10:48 +0000

    rm Provider Compoenents

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import "./App.css";
 import "react-toastify/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
-import { Web3ModalProvider } from "./Components/Home/web3/Provider";
+
 import Landing from "./Components/Home/Landing";
 function App() {
   return (
     <>
       <ToastContainer />
-      <Web3ModalProvider children={<Landing />} />
+      <Landing />
     </>
   );
 }
